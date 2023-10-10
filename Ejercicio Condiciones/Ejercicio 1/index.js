33a12fea9e4d1f6952d22a44e9faba88e0c091af
Almendra Castañeda <141966270+Almendrach24@users.noreply.github.com>
2023-10-09 22:37:58 -0300

    Add files via upload
    final condiciones

---
+++ Ejercicio Condiciones/Ejercicio 1/index.js
@@ -0,0 +1,15 @@
+const estilos = document.querySelector("#estilos");
+
+estilos.addEventListener("click", cambiarBorde);
+
+function cambiarBorde(){
+    estilos.classList.toggle('dibujo');
+}
+
+
+// document.getElementById("estilos").onclick = function (){
+//     cambiar();
+// }
+
+
+
